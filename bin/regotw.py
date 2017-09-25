7016aa02b8a84193dc45a0dcaaeb3a415ccde2ba
philsstein <phil.s.stein@gmail.com>
2017-09-25 21:28:06 +0000

    support for 5 year "redux, redux" posts added.

--- bin/regotw.py
+++ bin/regotw.py
@@ -52,49 +52,60 @@ if __name__ == '__main__':
     log.debug(u'sorted games: {}'.format(games))
 
     today = date.today()
+    # putting data here should, in theory, let us add other years easily. This is overkill. 
+    years_ago = {
+        2: {'date': None, 'game': None, 'as_string': 'two', 'title': 'Redux'},
+        5: {'date': None, 'game': None, 'as_string': 'five', 'title': 'Redux, Redux'}
+    }
     try:
-        two_years_ago = date(today.year-2, today.month, today.day)
+        for years in years_ago.keys():
+            years_ago[years]['date'] = date(today.year-years, today.month, today.day)
     except ValueError:   # triggered for feb 29th
-        two_years_ago = date(today.year-2, today.month+1, 1)
+        years_ago[years]['date'] = date(today.year-years, today.month+1, 1)
 
-    log.debug(u'2 years ago: {}'.format(two_years_ago))
+    log.debug(u'years ago: {}'.format(years_ago))
 
     for game in games:
-        if game[0] < two_years_ago:
-            log.debug(u'found GOTW older than two years: {}'.format(game[1]))
-        elif game[0] == two_years_ago:
-            log.debug(u'found two year old GOTW. Reposting {} GOTW post'.format(game[1]))
-            break
+        for ago in years_ago.keys():
+            if game[0] == years_ago[ago]['date']:
+                log.debug('Found game from {} years ago. Reposting.'.format(ago))
+                years_ago[ago]['game'] = game
+
+    if not any([years_ago[y]['game'] for y in years_ago.keys()]):
+        log.info('No games from {} found. Exiting.'.format(', '.join([str(y) for y in years_ago.keys()])))
+        exit(0)
+
+    for ago in years_ago.keys():
+        if not years_ago[ago]['game']:
+            continue
+
+        game = years_ago[ago]['game']
+        submission_id = game[2]
+        gotw_post = reddit.submission(id=submission_id)
+        print("GOTW: ", gotw_post, gotw_post.title)
+        search_str = u'\[//]: # \(GOTWS\)\s+(.+)\[//]: # \(GOTWE\)\s+'
+        m = re.search(search_str, gotw_post.selftext, flags=re.DOTALL)
+        if not m:
+            log.critical(u'Unable to find post body or body separator flags ([//]: # (GOTWS)...')
+            exit(1)
+
+        # figure suffix for the day for header inclusion.
+        if 4 <= game[0].day <= 20 or 24 <= game[0].day <= 30:
+            suffix = u"th"
         else:
-            log.debug(u'no GOTW from two years ago. Next GOTW repost is {} on {}'.format(
-                game[1], game[0]))
-            exit(0)
-    submission_id = game[2]
-    gotw_post = reddit.submission(id=submission_id)
-    print("GOTW: ", gotw_post, gotw_post.title)
-    search_str = u'\[//]: # \(GOTWS\)\s+(.+)\[//]: # \(GOTWE\)\s+'
-    m = re.search(search_str, gotw_post.selftext, flags=re.DOTALL)
-    if not m:
-        log.critical(u'Unable to find post body or body separator flags ([//]: # (GOTWS)...')
-        exit(1)
+            suffix = [u"st", u"nd", u"rd"][game[0].day % 10 - 1]
 
-    # figure suffix for the day for header inclusion.
-    if 4 <= game[0].day <= 20 or 24 <= game[0].day <= 30:
-        suffix = u"th"
-    else:
-        suffix = [u"st", u"nd", u"rd"][game[0].day % 10 - 1]
-
-    repost_header = u'''
-Note: {} was Game of the Week on /r/boardgames [two years ago today](/{}). This GoTW repost gives people of the sub a chance to discuss the game again after a bit of time has passed. Do you still play it? If so, how often does it make it to the table? Has the game held up after repeated plays? Has it moved up or down in your personal ranking? Has it been replaced by a newer, similar game? Has it replaced a game?
+        repost_header = u'''
+Note: {} was Game of the Week on /r/boardgames [{} years ago today](/{}). This GoTW repost gives people of the sub a chance to discuss the game again after a bit of time has passed. Do you still play it? If so, how often does it make it to the table? Has the game held up after repeated plays? Has it moved up or down in your personal ranking? Has it been replaced by a newer, similar game? Has it replaced a game?
 
 Below is [the original Game of the Week post](/{}) from {}:
 
 ------------------
 
-'''.format(game[1], game[2], game[2], game[0].strftime(u'%B %d{}, %Y'.format(suffix)))
+'''.format(game[1], years_ago[ago]['as_string'], game[2], game[2], game[0].strftime(u'%B %d{}, %Y'.format(suffix)))
 
-    repost_text = repost_header + m.group(1)
-    log.debug(u'Reposting this GOTW text:\n{}'.format(repost_text))
+        repost_text = repost_header + m.group(1)
+        log.debug(u'Reposting this GOTW text:\n{}'.format(repost_text))
 
-    title = u'Game of the Week, Redux: {}'.format(game[1])
-    repost = reddit.subreddit(subreddit).submit(title=title, selftext=repost_text)
+        title = u'Game of the Week, {}: {}'.format(years_ago[ago]['title'], game[1])
+        repost = reddit.subreddit(subreddit).submit(title=title, selftext=repost_text)
